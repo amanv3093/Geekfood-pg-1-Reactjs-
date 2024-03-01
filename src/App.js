a7619b777bc16b73432e74efd2fe109dd07f12e1
aman verma <amanv3093@gmai.com>
2024-03-01 18:59:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Herosection from './Components/Herosection';
+import './index.css';
+function App() {
+  return (
+    <div >
+     <Header />
+    <Herosection />
+    <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Herosection.jsx
@@ -0,0 +1,134 @@
+import React from 'react'
+
+function Herosection() {
+  return (
+    <>
+    <section style={{ backgroundImage: "url('https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')",height: 'calc(100vh - 72px)'}}className="bg-cover bg-center flex flex-col  justify-center items-starts" >
+    <div className='flex flex-col items-center w-[600px] '>
+     <h1 className='text-[40px] font-bold'>Let us find your</h1>
+     <h1 className='text-[40px] text-[#be123c] font-bold'>Forever Food.</h1>
+     <p className='text-[20px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+     <p className='text-[20px]'>Nesciunt illo tenetur fuga ducimus numquam ea!</p>
+
+     <div className='flex gap-3 pt-[2rem]'>
+      <button className='bg-[#be123c] hover:bg-[#a8304c] text-white px-[40px] py-[10px] rounded-md'>Search now</button>
+      <button className='bg-white text-[#be123c] px-[40px] py-[10px] rounded-md'>Know more</button>
+     </div>
+</div>
+
+
+    </section>
+
+<div className='flex justify-around items-center mt-[5rem]'>
+
+<img src='https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' className='w-[643px] h-[362px]'/>
+<div className='w-[500px]'>
+
+  <h1 className='text-2xl font-bold '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h1>
+
+  <p className='mt-5 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
+
+  <button className='bg-blue-700 py-[10px] px-[40px] text-white hover:bg-transparent hover:text-blue-700 border hover:border-blue-700 mt-5 rounded-md'>Get in Touch</button>
+</div>
+</div>
+
+<div className='flex justify-around flex-wrap mt-36'>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+
+
+
+</div>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae incidunt perferendis soluta facilis voluptas dicta repudiandae quasi asperiores libero, exercitationem molestiae autem sapiente dolore nulla non consequatur. Eaque, dolores.
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+</div>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi a voluptatum quidem nulla quisquam natus velit provident earum esse, odio numquam labore recusandae similique sunt.
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+</div>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vel quo deserunt quos expedita minima incidunt sed tempora, a architecto consectetur reprehenderit, in repellat voluptatum.
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+</div>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus eius aut unde, dolores accusantium!
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+</div>
+
+<div className='w-[400px] '>
+<div className=' rounded-xl bg-gray-50 p-6 shadow'>
+  <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ut necessitatibus, repudiandae qui dolor minima.
+  </p>
+  </div>
+  <div className='flex pt-4' >
+    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80 "
+    className='w-[48px] h-[48px] rounded-full' />
+    <span className='pl-4'>
+      <p className='text-sm font-bold'>Gladis Lennon</p>
+      <p className='text-sm '>Head of SEO</p>
+    </span>
+  </div>
+</div>
+</div>
+</>
+  )
+}
+
+export default Herosection;
